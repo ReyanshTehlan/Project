0070912a4f3a5606a72fcd120f7e5d902e9b64f6
ReyanshTehlan <127204356+ReyanshTehlan@users.noreply.github.com>
2023-06-02 16:08:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+heading = "My first console project"
+console.log(heading)
+first_name = "Reyansh"
+second_name = "Tehlan"
+full_name = first_name.concat(second_name)
+console.log(full_name)
+a = 102
+b = 24
+add=   a + b
+console.log(add)
+subtract=   a - b
+console.log(subtract)
+multiply=   a * b
+console.log(multiply)
+divide=   a / b
+console.log(divide)
